3a10c2fb45173359987b9e8c128b5ae543e2c3ad
mikowals <mikowals@gmail.com>
2013-07-10 11:33:34 +1000

    shared collection working and not in db

--- noMongo.js
+++ noMongo.js
@@ -1,14 +1,13 @@
-var linkedCollection = new Meteor.Collection( null ); // communicates but can not write to database.  See allow deny rules
 
 if (Meteor.isClient) {
-//  Meteor.subscribe( null );
+  var linkedCollection = new Meteor.Collection( 'publishedCollection' ); 
   Template.hello.greeting = function () {
     return linkedCollection.find({});
   };
 
+ Meteor.subscribe( 'publishedCollection' );   
   Template.hello.events({
     'click input' : function () {
-      // template data, if any, is available in 'this'
         var doc = linkedCollection.findOne({});
 	if( doc ) {
 	  Meteor.call( 'fakeUpdate', doc._id , {$inc: {clickCount: 1} }, function( error ){
@@ -17,54 +16,59 @@ if (Meteor.isClient) {
 	    }
 	  });
 	} else{
-	  linkedCollection.insert( {clickCount: 1} );
+	  Meteor.call( 'fakeInsert', {clickCount: 1} );
+	  linkedCollection.insert( {clickCount: 1});  // This will fail because linkedCollection on client does not have insert method.
 	}
+
     }
   });
 };
 
+
+
 Meteor.methods({
+
+//shared code so method runs on client as stub and on server
+////linkedCollection is a trick because both client and server each have a local collection with that name
+
   fakeUpdate: function( id, modifier, cb ){
-    var self = this;
+  /*  var self = this;
     if ( self.isSimulation ){
-      linkedCollection.update( id, modifier );
+      clientCollection.update( id, modifier );
     } else{
-      linkedCollection.update( id, modifier, cb );
+      serverCollection.update( id, modifier, cb );
+    }*/
+    linkedCollection.update( id, modifier, cb);
+  },
+
+
+  fakeInsert: function( doc ){
+    linkedCollection.insert( doc );
+    },
+
+  fakeRemove: function ( id ){
+    if (check( id, String )){
+      linkedCollection.remove( doc._id );
     }
   }
 });
 
 if (Meteor.isServer) {
-  var noMongo = new Meteor.Collection( null ); 
+  var linkedCollection = new Meteor.Collection( null ); // communicates but can not write to database.  See allow deny rules
   
-  linkedCollection.allow({
-    insert: function( userId, doc )  { 
-      noMongo.insert( doc ); // can be done here but might be cleaner to make a fakeInsert method;
-      return true;
-    },
-    
-    update: function(userId, doc, fields, modifier) {
-      return true;
-    },
-    
-    remove: function(userId, doc) {
-      return true;
-    }
-  });
-
-  Meteor.publish( null , function() {
+  Meteor.publish( 'publishedCollection' , function() {
     var self = this;
     var handle = linkedCollection.find({}).observe({
 
       added: function (doc){
-	self.added( null , doc._id, doc );
+	self.added( 'publishedCollection' , doc._id, doc );
       },
       removed: function ( doc ){
-	self.removed( null , doc._id );
+	self.removed( 'publishedCollection' , doc._id );
       },
     
       changed: function ( doc ){
-	self.changed( null , doc._id, doc );
+	self.changed( 'publishedCollection' , doc._id, doc );
       }
     });
     
@@ -73,15 +77,7 @@ if (Meteor.isServer) {
     });
     
     self.ready();
-/**
-    var published = linkedCollection.find({});
-    console.log( published );
-    return published;
-    **/
+    
   });
   
-  Meteor.startup(function () {
-    linkedCollection.insert( {_id: "007", clickCount: 10});
-    // code to run on server at startup
-  });
-}
+};
