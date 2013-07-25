9e213b7126ca2cb18cb805820c1b3cadc1dc166a
mikowals <mikowals@gmail.com>
2013-07-25 14:13:19 +1000

    preparing to merge

--- noMongo.js
+++ noMongo.js
@@ -1,11 +1,13 @@
 
 if (Meteor.isClient) {
   var linkedCollection = new Meteor.Collection( 'publishedCollection' ); 
+
   Template.hello.greeting = function () {
     return linkedCollection.find({});
   };
 
- Meteor.subscribe( 'publishedCollection' );   
+ // Meteor.subscribe( 'publisher' );   
+  
   Template.hello.events({
     'click input' : function () {
         var doc = linkedCollection.findOne({});
@@ -13,23 +15,22 @@ if (Meteor.isClient) {
 	  Meteor.call( 'fakeUpdate', doc._id , {$inc: {clickCount: 1} }, function( error ){
 	    if( error ){
 	      console.log ( "error from update: " + error);
+	    
 	    }
 	  });
+	  // linkedCollection.update( doc._id, {$inc: {clickCount: 1} } ); // This will fail because linkedCollection does not have update Metoer method.
 	} else{
 	  Meteor.call( 'fakeInsert', {clickCount: 1} );
-	  linkedCollection.insert( {clickCount: 1});  // This will fail because linkedCollection on client does not have insert method.
+	  //linkedCollection.insert( {clickCount: 1});  // This will fail because linkedCollection does not have insert Meteor method.
 	}
-
     }
   });
 };
 
-
-
 Meteor.methods({
 
-//shared code so method runs on client as stub and on server
-////linkedCollection is a trick because both client and server each have a local collection with that name
+//shared code so method runs on client as stub and on server with access to server local collection.
+////linkedCollection is a trick because the  client and server each have their own collection with that name
 
   fakeUpdate: function( id, modifier, cb ){
   /*  var self = this;
@@ -43,20 +44,25 @@ Meteor.methods({
 
 
   fakeInsert: function( doc ){
-    linkedCollection.insert( doc );
-    },
+    var self = this;
+    if (self.isSimulation){
+      doc._id = linkedCollection.insert( doc ); // without this ids can get out of sync.  With it server may receive duplicate id.
+    } else{
+      linkedCollection.insert( doc );
+    }
+  },
 
   fakeRemove: function ( id ){
-    if (check( id, String )){
+    if (check( id, String )){ // this check prevents client from breaking 1 document at a time rule
       linkedCollection.remove( doc._id );
     }
   }
 });
 
 if (Meteor.isServer) {
-  var linkedCollection = new Meteor.Collection( null ); // communicates but can not write to database.  See allow deny rules
+  var linkedCollection = new Meteor.Collection( null ); //local collection because of null name
   
-  Meteor.publish( 'publishedCollection' , function() {
+  Meteor.publish( null , function() {
     var self = this;
     var handle = linkedCollection.find({}).observe({
 
@@ -75,9 +81,11 @@ if (Meteor.isServer) {
     self.onStop( function (){
       handle.stop();
     });
-    
+     
+    var newId = linkedCollection.insert(  { guestId: linkedCollection.find({}).count() + 1, clickCount: 0}  );    
+    var newDoc = linkedCollection.findOne(newId );
+    self.changed( 'publishedCollection' , newDoc._id, {guestId: newDoc.guestId, clickCount: newDoc.clickCount, privateMessage:"a secret message to " +  newDoc.guestId} );
     self.ready();
-    
   });
   
 };
